25bd5093a4d84bc8f0520fec9949486972644bd5
Vladislav Kashin <limon4ik.mail@gmail.com>
2025-05-04 12:14:50 +0300

    Bugfix

--- Assets/Scripts/UI/LevelUpWindow.cs
+++ Assets/Scripts/UI/LevelUpWindow.cs
@@ -8,12 +8,14 @@ namespace UI
 { 
     public class LevelUpWindow : Window
     {
+        private const int CountItems = 3;
+
         [SerializeField] private List<ItemView> _itemsVisual;
         [SerializeField] private ItemApplicator _applicator;
 
         public void ShowWithItems(List<ItemVisualData> items)
         {
-            for (int i = 0; i < items.Count; i++)
+            for (int i = 0; i < CountItems; i++)
                 _itemsVisual[i].Initialize(items[i]);
 
             _applicator.SetdDefaultItem(items.First());
