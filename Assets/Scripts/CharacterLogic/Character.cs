150acbd6e37613f6896b21d105858a8cdbbee98a
NikitaAver <nikita.averchenko1@gmail.com>
2026-08-02 15:05:48 +0300

    Idamagable itegrated

--- Assets/Scripts/CharacterLogic/Character.cs
+++ Assets/Scripts/CharacterLogic/Character.cs
@@ -328,7 +328,7 @@ namespace CharacterLogic
             if (_characterCanvas != null) _characterCanvas.gameObject.SetActive(true);
         }
 
-        public void TakeDamage(float damage)
+        public void TakeDamage(float damage, bool isFromBerserk = false)
         {
             if (_isInvincible) return;
             CameraShake.Instance.ShakeCamera(2, 5, 0.3f);
